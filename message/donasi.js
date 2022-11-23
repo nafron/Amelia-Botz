3e387ebc34b9ce55b072c522cf9550520b9d3986
nafron <99122428+nafron@users.noreply.github.com>
2022-11-23 10:12:37 +0800

    Update donasi.js

--- message/donasi.js
+++ message/donasi.js
@@ -1,18 +1,11 @@
 exports.dodo = (prefix) => {
 return`
-*╭─▸*「 *DONASI* 」
+*╭─▸*「 *GAK PERLU DONASI* 」
 *┴*
-*│* Bantu Developer ngembangin bot ini
-*│* dengan donasi seiklasnya 🙂
-*│*
-*│* Shopee : 085156137901
-*│* Gopay : 085156137901
-*│* Dana : 085156137901
-*│* Ovo : 085156137901
-*│*
-*│* Makasih yang udah donasi
-*│* dan bantu ngembangin bot ini
-*│* moga rejekinya lancar amin
+*│* Cukup follow tiktok admin
+*│* sebagai Bentuk Dukungan
+*│* tiktok : https://tiktok.com/@nafronanjay2
+*│* Makasih yang udah follow
 *┬*
 *╰────────────[*`
 }
